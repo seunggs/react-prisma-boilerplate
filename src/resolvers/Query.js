9e6809f9a6e58bc6052e2abe2dd602e8365cbda2
Seungchan Lee <seungchanlee@Seungchans-MacBook-Pro-2.local>
2019-03-04 13:21:59 -0800

    minor fix

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,20 +1,10 @@
 import * as R from 'ramda'
 import UserQuery from './UserQuery'
 import FileQuery from './FileQuery'
-import ProjectQuery from './ProjectQuery'
-import MemberQuery from './MemberQuery'
-import PostQuery from './PostQuery'
-import CommentQuery from './CommentQuery'
-import MilestoneQuery from './MilestoneQuery'
 
 const Query = R.mergeAll([
   UserQuery,
   FileQuery,
-  ProjectQuery,
-  MemberQuery,
-  PostQuery,
-  CommentQuery,
-  MilestoneQuery,
 ])
 
 export { Query as default }
